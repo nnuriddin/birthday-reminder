80a26d7d733985058864ae6b407f3745dbc2d835
nnuriddin <nuriddindoniyorov2@gmail.com>
2022-03-16 08:48:12 +0500

    birthday reminder project with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+
+import data from "./data";
+import List from "./components/Lists";
+
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(data);
+
+  const changingPeopleArr = () => {
+    setPeople([]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={changingPeopleArr}>claer all</button>
+      </section>
+    </main>
   );
 }
 
